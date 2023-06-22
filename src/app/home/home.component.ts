45df660731f4769b5c5d87ea949131200f0a48fc
Manhduc13 <manhduc130702@gmail.com>
2023-06-22 11:03:00 +0700

    import imput to housing location.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { Housinglocation } from '../interface/housinglocation';
     </form>
   </section>
   <section class="results">
-    <app-housing-location></app-housing-location>
+    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
   </section>
   `,
   styleUrls: ['./home.component.css']
