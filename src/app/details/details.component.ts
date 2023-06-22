7ecdd891875a82a6591dfc72dbfc9df7f511061a
Manhduc13 <manhduc130702@gmail.com>
2023-06-22 16:40:23 +0700

    add route link

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../interface/housinglocation';
+import { HousingService } from '../housing-location/housing.service';
 @Component({
   selector: 'app-details',
-  templateUrl: './details.component.html',
+  template: `
+  <article>
+    <img class="listing-photo" [src]="housingLocation?.photo"
+      alt="Exterior photo of {{housingLocation?.name}}"/>
+    <section class="listing-description">
+      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.nation}}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{housingLocation?.availableUnits}}</li>
+        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+      </ul>
+    </section>
+  </article>
+`,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingService = inject(HousingService);
+    housingLocation: HousingLocation | undefined;
+  
+    constructor() {
+      const housingLocationId = Number(this.route.snapshot.params['id']);
+      this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,6 +7,7 @@ import { HousingLocation } from '../interface/housinglocation';
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}} | {{housingLocation.nation }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">More Info</a>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
